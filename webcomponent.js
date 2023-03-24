f042c382b5ea8176b4738c974ab2ded499a69d57
Alexander Bauer <alexander.bauer@bauerberatung.de>
2023-03-24 09:54:28 +0100

    Update

--- webcomponent.js
+++ webcomponent.js
@@ -5,45 +5,33 @@
 
   let tmpl = document.createElement("template");
   tmpl.innerHTML = `
-        <style>
-        </style>
-        <div id="ui5_content" name="ui5_content">
-         <slot name="content"></slot>
-        </div>
+    <style>
+    </style>
 
-        <script id="oView" name="oView" type="sapui5/xmlview">
-            <mvc:View
-			    controllerName="com.snp.materialselect"
-				xmlns:l="sap.ui.layout"
-				xmlns:mvc="sap.ui.core.mvc"
-				xmlns="sap.m">
-				<l:VerticalLayout
-					class="sapUiContentPadding"
-					width="100%">
-					<l:content>
-                        <Button text="Press me" id="btnTest press="onButtonPress" />
-					</l:content>
-				</l:VerticalLayout>
-			</mvc:View>
-        </script>        
+    <div id="ui5_content" name="ui5_content">
+      <slot name="content"></slot>
+    </div>
+
+    <script id="oView" name="oView" type="sapui5/xmlview">
+      <mvc:View
+        controllerName="com.snp.materialselect"
+        xmlns:l="sap.ui.layout"
+       xmlns:mvc="sap.ui.core.mvc"
+        xmlns="sap.m">
+        <l:VerticalLayout
+        class="sapUiContentPadding"
+        width="100%">
+        <l:content> 
+          <Button text="Press me" id="btnTest press="onButtonPress" />
+        </l:content>
+        </l:VerticalLayout>
+       </mvc:View>
+    </script> 
     `;
 
   class SNPMaterialSelect extends HTMLElement {
     constructor() {
       super();
-
-      _shadowRoot = this.attachShadow({
-        mode: "open",
-      });
-      _shadowRoot.appendChild(tmpl.content.cloneNode(true));
-
-      // _id = createGuid();
-
-      // _shadowRoot.querySelector("#oView").id = _id + "_oView";
-      _shadowRoot.querySelector("#oView").id = "_oView";
-
-      this._export_settings = {};
-      this._export_settings.password = "";
 
       this.addEventListener("click", (event) => {
         console.log("click");
@@ -58,38 +46,6 @@
 
     onCustomWidgetAfterUpdate(changedProperties) {
       loadthis(this);
-    }
-
-    _firePropertiesChanged() {
-      this.password = "";
-      this.dispatchEvent(
-        new CustomEvent("propertiesChanged", {
-          detail: {
-            properties: {
-              password: this.password,
-            },
-          },
-        })
-      );
-    }
-
-    // SETTINGS
-    get password() {
-      return this._export_settings.password;
-    }
-    set password(value) {
-      value = _password;
-      this._export_settings.password = value;
-    }
-
-    static get observedAttributes() {
-      return ["password"];
-    }
-
-    attributeChangedCallback(name, oldValue, newValue) {
-      if (oldValue != newValue) {
-        this[name] = newValue;
-      }
     }
   }
 
@@ -127,9 +83,10 @@
         });
       });
 
+      debugger;
       //### THE APP: place the XMLView somewhere into DOM ###
       var oView = sap.ui.xmlview({
-        viewContent: jQuery(_shadowRoot.getElementById(_id + "_oView")).html(),
+        viewContent: jQuery(document.getElementById("_oView")).html(),
       });
       oView.placeAt(content);
 
