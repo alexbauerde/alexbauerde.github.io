4ab01b6f3d50dd5a2e341d66e420f5aa724d3c90
Alexander Bauer <alexander.bauer@bauerberatung.de>
2023-03-24 09:50:40 +0100

    Update

--- webcomponent.js
+++ webcomponent.js
@@ -37,9 +37,10 @@
       });
       _shadowRoot.appendChild(tmpl.content.cloneNode(true));
 
-      _id = createGuid();
+      // _id = createGuid();
 
-      _shadowRoot.querySelector("#oView").id = _id + "_oView";
+      // _shadowRoot.querySelector("#oView").id = _id + "_oView";
+      _shadowRoot.querySelector("#oView").id = "_oView";
 
       this._export_settings = {};
       this._export_settings.password = "";
@@ -47,92 +48,6 @@
       this.addEventListener("click", (event) => {
         console.log("click");
       });
-    }
-
-    connectedCallback() {
-      try {
-        if (window.commonApp) {
-          let outlineContainer = commonApp.getShell().findElements(true, (ele) => ele.hasStyleClass && ele.hasStyleClass("sapAppBuildingOutline"))[0]; // sId: "__container0"
-
-          if (outlineContainer && outlineContainer.getReactProps) {
-            let parseReactState = (state) => {
-              let components = {};
-
-              let globalState = state.globalState;
-              let instances = globalState.instances;
-              let app = instances.app['[{"app":"MAIN_APPLICATION"}]'];
-              let names = app.names;
-
-              for (let key in names) {
-                let name = names[key];
-
-                let obj = JSON.parse(key).pop();
-                let type = Object.keys(obj)[0];
-                let id = obj[type];
-
-                components[id] = {
-                  type: type,
-                  name: name,
-                };
-              }
-
-              for (let componentId in components) {
-                let component = components[componentId];
-              }
-
-              let metadata = JSON.stringify({
-                components: components,
-                vars: app.globalVars,
-              });
-
-              if (metadata != this.metadata) {
-                this.metadata = metadata;
-
-                this.dispatchEvent(
-                  new CustomEvent("propertiesChanged", {
-                    detail: {
-                      properties: {
-                        metadata: metadata,
-                      },
-                    },
-                  })
-                );
-              }
-            };
-
-            let subscribeReactStore = (store) => {
-              this._subscription = store.subscribe({
-                effect: (state) => {
-                  parseReactState(state);
-                  return {
-                    result: 1,
-                  };
-                },
-              });
-            };
-
-            let props = outlineContainer.getReactProps();
-            if (props) {
-              subscribeReactStore(props.store);
-            } else {
-              let oldRenderReactComponent = outlineContainer.renderReactComponent;
-              outlineContainer.renderReactComponent = (e) => {
-                let props = outlineContainer.getReactProps();
-                subscribeReactStore(props.store);
-
-                oldRenderReactComponent.call(outlineContainer, e);
-              };
-            }
-          }
-        }
-      } catch (e) {}
-    }
-
-    disconnectedCallback() {
-      if (this._subscription) {
-        this._subscription();
-        this._subscription = null;
-      }
     }
 
     onCustomWidgetBeforeUpdate(changedProperties) {
@@ -177,6 +92,7 @@
       }
     }
   }
+
   customElements.define("com-snp-materialselect", SNPMaterialSelect);
 
   // UTILS
